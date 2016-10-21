dbc2190e5ee2462c8684fbe83ea79f05f93f0aa5
GM <GIORGOSMOUS@gms-macbook-pro.local>
2016-10-21 23:21:21 +0300

    Initial Commit

---
+++ ncognito/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ncognito
+//
+//  Created by GM on 21/10/2016.
+//  Copyright © 2016 Georgios Moustakas. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
